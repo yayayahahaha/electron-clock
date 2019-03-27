36498b6e9737e41c0218957bbdb72f42dac2c125
flyc <yayayahahahaooii@gmail.com>
2019-03-27 14:02:05 +0800

    clean up app.js

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-const moment = require('moment')
 const fs = require('fs')
 const axios = require('axios')
-
-const nowDom = document.querySelector('.current-time')
-const alarmDom = document.querySelector('.alarm-time')
-
-
-// alarmDom.addEventListener('change', onAlarmTextChange)
-
-let time = moment()
-
-let nowTime
-let alarmTime
-
-var timmer = setInterval(function() {
-    const time = moment().format('HH:mm:ss')
-    nowDom.innerText = time
-}, 300);
-
-const db = fs.readFileSync('db.json')
-let json = JSON.parse(db)
-json.hello = 'there'
-
-fs.writeFileSync('db.json', JSON.stringify(json, null, 2))
-
 
 /* ============================================================== */
 // notification
